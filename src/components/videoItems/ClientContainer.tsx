c15e88384f8792dd70260a4ba80e61ddfb466038
Kerby Keith Aquino <hello@kurojifusky.com>
2025-01-14 05:11:54 +0800

    implement videoItemGrid dummy

--- src/components/videoItems/ClientContainer.tsx
+++ src/components/videoItems/ClientContainer.tsx
@@ -1,6 +1,8 @@
 "use client"
 
 import { useViewItemStateContext, type ViewItemContext } from "@/context"
+import { cn } from "@/utils"
+import VideoItemGrid from "./VideoItemGrid"
 
 interface VideoItemContainerProps {
   queryView?: ViewItemContext["view"]
@@ -9,5 +11,11 @@ interface VideoItemContainerProps {
 export function VideoItemContainer(props: VideoItemContainerProps) {
   const { view, setView } = useViewItemStateContext()
 
-  return <div>{`The damn view is ${view} boi`}</div>
+  return (
+    <div className={cn("grid gap-1.5", view === "grid" ? "grid-cols-4" : "")}>
+      {[...Array(21)].map((_, i) => (
+        <VideoItemGrid key={i} />
+      ))}
+    </div>
+  )
 }
---
+++ src/components/videoItems/VideoItemCompact.tsx
@@ -0,0 +1,5 @@
+import { SharedVideoItemProps } from "./VideoItem.types"
+
+export default function VideoItemCompact(props: SharedVideoItemProps) {
+  return <></>
+}
