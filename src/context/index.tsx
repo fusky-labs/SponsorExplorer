f24c99044689fc2ed8f1da722b259be9a2e72382
Kerby Keith Aquino <hello@kurojifusky.com>
2025-01-16 12:24:26 +0800

    update pos

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -2,6 +2,7 @@ export * from "./TabStateProvider"
 export * from "./ViewItemProvider"
 
 export { useVideoSidebarContext } from "./video/VideoSidebarProvider"
-export * from "./video/VideoInfoProvider"
+
 export * from "./TimelineClientProvider"
+export * from "./video/VideoInfoProvider"
 export * from "./video/LiveSegmentProvider"
